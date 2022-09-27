978a70776fde0810e6f9149dd9fbfaa39151167a
Doug Sellers <DouglasSSellers@gmail.com>
2022-09-26 21:53:37 -0400

    added deBounded effect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,91 +1,102 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('programming')
-    const [results, setResults] = useState([])
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-    
-    // useEffect(() => {
-    //     const search = async ()=> {
-    //        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-    //             params: {
-    //                action: 'query',
-    //                list: 'search', 
-    //                origin: '*',
-    //                format: 'json',
-    //                srsearch: term,
-    //             }
-    //         })
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-    //         setResults(data.query.search)
-    //     }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-    //     const timeoutId = setTimeout(() => {
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm,
+        },
+      });
 
-    //         if (term) {
-    //             search()
-    //         }
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
-    //     }, 500)
-    // }, [term])
+  //   useEffect(() => {
+  //     const search = async () => {
+  //       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+  //         params: {
+  //           action: 'query',
+  //           list: 'search',
+  //           origin: '*',
+  //           format: 'json',
+  //           srsearch: term,
+  //         },
+  //       });
 
-    useEffect(() => {
-       console.log('CLEANUP')
+  //       setResults(data.query.search);
+  //     };
 
-       const search = async ()=> {
-               const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                    params: {
-                       action: 'query',
-                       list: 'search', 
-                       origin: '*',
-                       format: 'json',
-                       srsearch: term,
-                    }
-                })
-    
-                setResults(data.query.search)
-            }
+  //     if (term && !results.length) {
+  //       search();
+  //     } else {
+  //       const timeoutId = setTimeout(() => {
+  //         if (term) {
+  //           search();
+  //         }
+  //       }, 1000);
 
-       return () => {
-          return search
-       }
-    }, [term])
+  //       return () => {
+  //         clearTimeout(timeoutId);
+  //       };
+  //     }
+  //   }, [term, results.length]);
 
-    const renderedResults = results.map((result) => {
-        return (
-            <div key={result.pageid} className='item'>
-                <div className='right floated content'>
-                    <a 
-                    className='ui button'
-                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
-                    >Go</a>
-                </div>
-                <div className='content'>
-                    <div className='header'>
-                      {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
-                </div>
-            </div>
-        )
-    })
-
+  const renderedResults = results.map((result) => {
     return (
-        <div>
-            <div className='ui form'>
-                <div className='field'>
-                    <label>Enter Search Term</label>
-                    <input 
-                    value={term}
-                    onChange={(e) => setTerm(e.target.value)}
-                    className='input' 
-                    />
-                </div>
-            </div>
-            <div className='ui celled list'>{renderedResults}</div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
-    )
-}
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
-export default Search
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
